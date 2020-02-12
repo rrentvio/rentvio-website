02fd6cc67b9222df87c9788f6508cfda93812254
Mustafa Beyceoğlu <42028573+Mbeyceoglu@users.noreply.github.com>
2020-02-12 16:23:26 +0300

    Index.php
    Still trying to learn how to commit :angry:

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rentvio</title>
+</head>
+<body>
+<h>Hello World (Wide Web)!</h>
+<br>
+</body>
+</html>
+
+<?php
+
+echo"Fun Fact: 'www' stands for world wide web";
+?>
+
