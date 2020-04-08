734efa8e74d6322aa263d219863233e6c1c50b28
Mustafa Beyceoğlu <42028573+Mbeyceoglu@users.noreply.github.com>
2020-04-08 04:42:54 +0300

    Latest Changes

---
+++ application/controllers/Users.php
@@ -0,0 +1,17 @@
+<?php
+
+Class Users extends CI_Controller{
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+
+        echo "çakmacıyız.net";
+
+    }
+
+}
