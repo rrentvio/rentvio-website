44a21ba181fe857ae01ab05dc8b5af763d09238b
Mustafa Beyceoğlu <42028573+Mbeyceoglu@users.noreply.github.com>
2020-05-09 05:51:56 +0300

    addproducts basic shape is done

--- application/views/addproduct.php
+++ application/views/addproduct.php
@@ -16,17 +16,17 @@
 <div>
 <form>
   <div class="form-group">
-    <label for="productName">Product name</label>
+    <label for="productName"> <b> <i>Product name</i></b> </label>
     <input type="text" class="form-control" id="pName" placeholder="Enter Product Name">
   </div>
   
   <div class="form-group">
-    <label for="productDescription">Product Description</label>
+    <label for="productDescription">  <b> <i>Product Description </i></b></label>
     <textarea class="form-control" id="productDescription" rows="3"></textarea>
   </div>
-
+    
   <div class="form-group">
-    <label for="categoryChooser">Choose Categroy</label>
+    <label for="categoryChooser"> <b><i>Choose Categroy </i></b> </label>
     <select class="form-control" id="categoryChooser">
         <option value="1">Fotoğraf & Kamera </option>
         <option value="2">Kitap Dergi       </option>
@@ -36,20 +36,19 @@
         <option value="6">Diğer Herşey      </option>   
     </select>
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="publish">
-    <label class="form-check-label" for="publish">Publish item</label>
-  </div>
-  <form>
+  <div class="custom-control custom-switch">
+  <input type="checkbox" class="custom-control-input" id="publish">
+  <label class="custom-control-label" for="publish"> <b><i>Publish when appored </i></b> </label>
+    </div>
+    <br>
   <div class="form-group">
-    <label for="imageInput">Enter product Image</label>
+    <label for="imageInput">         <b><i>Enter product Image  </i></b>       </label>
     <input type="file" class="form-control-file" id="imageInput">
   </div>
-</form>
+
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
     
-
 </body>
 </html>
