95773528313b6a24a6ae0d6dd336538b2910632b
Mustafa Beyceoğlu <42028573+Mbeyceoglu@users.noreply.github.com>
2020-05-14 01:41:37 +0300

    Update 14.05.2020.sql

--- sqlfiles/14.05.2020.sql
+++ sqlfiles/14.05.2020.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 14 May 2020, 00:02:41
+-- Üretim Zamanı: 14 May 2020, 00:41:24
 -- Sunucu sürümü: 10.4.11-MariaDB
 -- PHP Sürümü: 7.2.28
 
@@ -21,6 +21,33 @@ SET time_zone = "+00:00";
 --
 -- Veritabanı: `rentvio`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(50) COLLATE utf8mb4_turkish_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_turkish_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8mb4_turkish_ci NOT NULL,
+  `user_name` varchar(50) COLLATE utf8mb4_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+
+--
+-- Tablo döküm verisi `users`
+--
+
+INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `user_name`) VALUES
+(1, 'Mustafa Beyceoğlu', 'mustafa.beyceoglu@std.antalya.edu.tr', '81dc9bdb52d04dc20036dbd8313ed055', 'Mbeyceoglu'),
+(2, 'Berke Çakıroğlu', 'berke.cakiroglu@std.antalya.edu.tr', '81dc9bdb52d04dc20036dbd8313ed055', 'Bcakiroglu'),
+(3, 'Logan Gomez', 'logan.gomez@std.antalya.edu.tr', '81dc9bdb52d04dc20036dbd8313ed055', 'Lgomez'),
+(6, 'New User', 'new.user@std.antalya.edu.tr', '81dc9bdb52d04dc20036dbd8313ed055', 'Nuser'),
+(8, 'New Usertwo', 'new.usertwo@std.antalya.edu.tr', '81dc9bdb52d04dc20036dbd8313ed055', 'Nusertwo'),
+(12, 'Deneme User', 'deneme@email.com', '8f10d078b2799206cfe914b32cc6a5e9', 'deneme'),
+(13, 'Muazzez Zehra ', 'muazzez.zehra@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'Mzehre');
 
 -- --------------------------------------------------------
 
@@ -70,6 +97,12 @@ INSERT INTO `user_product` (`id`, `user_id`, `product_name`, `price`, `product_d
 --
 
 --
+-- Tablo için indeksler `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Tablo için indeksler `user_product`
 --
 ALTER TABLE `user_product`
@@ -78,6 +111,12 @@ ALTER TABLE `user_product`
 --
 -- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
 --
+
+--
+-- Tablo için AUTO_INCREMENT değeri `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `user_product`
