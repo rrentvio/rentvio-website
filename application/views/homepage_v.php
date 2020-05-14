1b4737938b3761684f35174f3ac66e978e73a828
Berke Cakiroglu <54314734+BerkeCakiroglu@users.noreply.github.com>
2020-05-14 05:02:05 +0300

    database correction

--- application/views/homepage_v.php
+++ application/views/homepage_v.php
@@ -106,7 +106,7 @@
                         <div class="jumbotron p-3 text-deneme-bg text-white text-center">
                           <div class="row ">
                             <div class="col-md-8 offset-1 ">
-                              <img src="<?php echo base_url("assets/pictures/Z.png");?>" alt="" class="no-image">
+                              <img src="<?php if(isset($product->product_picture)){echo $product->product_picture;} else echo base_url("assets/pictures/Z.png");?>" alt="" class="no-image">
                               <br><br>
                               <h6 class="text-light text-left"><?php echo $product->product_name?></h6>
                               <hr>
