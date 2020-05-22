8465e30fbfffd91def4f20c86b4079d1d149682f
Berke Cakiroglu <54314734+BerkeCakiroglu@users.noreply.github.com>
2020-05-23 02:42:31 +0300

    product id_v

--- application/views/product_v.php
+++ application/views/product_v.php
@@ -4,62 +4,108 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?php echo(reset($product_details)->product_name);?> </title>
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css");?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/custom.css");?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/custom2.css");?>">
 </head>
 <body>
-    <h1>
-    <?php 
-    if (isset($product_details)){
-        $product_details= reset($product_details);
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark mb-10 mt-10 position-fixed wwww">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <?php 
+            if (isset($user -> email)){
+              echo'<a class="nav-link diffont h3 m-0 m-0 text-deneme " style="  padding: 0px 5px 0px 0px" href=';
+              echo base_url("homepage/".md5($user -> email));   echo("  >Rentvio<</a>");
+            }
+
+            else{
+              echo'<a class="nav-link text-bold diffont h3 m-0  text-deneme " style=" padding: 0px 5px 0px 0px" href=';
+              echo base_url()." > Rentvio<</a>";
+            }
+            ?>
+        </li>
+        <li class="nav-item">
+            <?php 
+            if (isset($user -> email)){
+              echo '<a class="nav-link fancy-font " href= ';
+              echo base_url("profile/" .md5($user -> email)); echo ">",$user->full_name,"</a>";  ?> 
+            <?php
+            }
+            else{              
+              }
+            ?>
+
+        </li>
+        <li class="nav-item">
+            <div class="dropdown">
+              <button class="btn btn-outline-light fancy-font dropdown-toggle col-12 xd" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                All Products
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item fancy-font" href="#">Photograph and Video</a>
+                <a class="dropdown-item fancy-font" href="#">Book and Magazines</a>
+                <a class="dropdown-item fancy-font" href="#">Sport Equitments</a>
+                <a class="dropdown-item fancy-font" href="#">Home Improvemnet and Tools</a>
+                <a class="dropdown-item fancy-font" href="#">Technical Electronics</a>
+                <a class="dropdown-item fancy-font" href="#">Other Stuff</a>
+              </div>
+            </div>
+          </li>
+          <li>
+            <form class="form-inline my-2 my-lg-0">
+        <input class="form-control fancy-font mr-sm-2 ml-3" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline fancy-font text-deneme-bg text-white my-2 ml-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </li>
+          
+    </ul>  
+
+
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item text-deneme-bg active ">
         
-        //berke bunları takıl işte resim için bir slayt tarzı bişe yap + bu verileri kullanarak sayfayı tasarla 
-        $id=$product_details->id;
-        $postedby=$product_details->user_id;
-        $name=$product_details->product_name;
-        $price=$product_details->price;
-        $description=$product_details->product_description;
-        $category=$product_details->product_category;
-        $renterid=$product_details->renter_id;
-        $uploaddate=$product_details->upload_date;
-        echo("id= ".$id."<br>");
-        echo("Posted by (user id )= ".$postedby." <br> ");
-        echo("fiyat bilgisi= ".$price."<br>");
-        echo("açıklama= ".$description."<br>");
-        echo("bunu kullanmayacağız büyük ihtimalle ama kiralayan kişinin is si = ".$renterid."<br>");
-        echo("yüklenme tarihi= ".$uploaddate."<br>");
-        $uploader= $user->full_name;
-        echo("Yükleyen kişi name i (başka bir veri lazım sa bir üst line a bak...)= ".$uploader."<br>");
-        //print_r($product_images);
-        echo "<br><br><br>";
-        //bir sayfa geriye gitmek amaçlı script için şu partı silmemen lazım = onclick="goBack()"
-        echo '<button onclick="goBack()" >Go Back </button>';
-        
+        <?php 
+            if (isset($user -> email)){ 
+              echo'<a class="nav-link fancy-font bgbetter" href=';
+              echo base_url("logout/" .md5($user -> email));?>  <?php echo">Log out</a>"; ?>
+            <?php
+            }
+            else{
+              echo'<a  class="nav-link fancy-font bgbetter" href="" data-toggle="modal" data-target="#signinModal" data-whatever="buraya istediğin bir veriyi gir"  id="sign"> Log in</a>';
+              //echo base_url("giris"); echo"> Signin</a>"; 
+              }
+            ?>
+            <a  class="nav-link fancy-font d-none " data-toggle="modal" data-target="#signUp" data-whatever="buraya istediğin bir veriyi gir"  id="signup"> Sign in</a>
+            </li>
+    </ul>
+  </nav>
 
 
 
-        //örnek resim çekme kodu
-        echo "<br><br><br>";
-        echo "örnek resim çekme kodu";
-        echo "<br>";
-        foreach($product_images as $image){
-            echo "<br><br><br>";
-            echo '<img src="'.$image->pic_url.'" alt="">';
-        }
-    }
-    else{
-    echo"Product not found!";
-    sleep(5);
-    echo"Redirecting to home page";
-    redirect(base_url());
-    }
-    ?>
-    </h1>
-    
-        
-</body>
-</html>
+  
 
-<script>
+
+ 
+<script
+      src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+      crossorigin="anonymous"
+    ></script>
+    <script>
 function goBack() {
   window.history.back();
 }
 </script>
+</body>
+</html>
+
